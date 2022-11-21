60eded419d0d2fc6bd9b00121d2097d3a4ea3725
ZeroTwo5166 <subarna.gurung23@gmail.com>
2022-11-21 20:01:06 +0100

    Commited all

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.css'
+
+const Button = (props) => {
+  return (
+    <button className={props.className} onClick={props.onClick}>{props.value}</button>
+  )
+}
+
+export default Button
